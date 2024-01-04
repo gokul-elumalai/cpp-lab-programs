778d3f2714acccc5a28a74e08ec2003f00252a41
gokul-elumalai <src.mv1f3@aleeas.com>
2024-01-04 08:53:33 +0530

    Add enum program

---
+++ enums.cpp
@@ -0,0 +1,18 @@
+// Enums
+
+# include<iostream.h>
+# include<conio.h>
+
+enum bool { true, false };
+
+int main() {
+  clrscr();
+  bool b;
+
+  b = true;
+
+  cout << "bools is: " << b << endl;
+
+  getch();
+  return 0;
+}
